c1f983ec4def3613ba910a00bdbd34c6a5008413
Tommi Oinonen <tommi.oinonen@gmail.com>
2019-06-13 11:03:53 +0300

    adds a helper script for parsing the contexts from git diffs

---
+++ helpers/diff2change_context_list.py
@@ -0,0 +1,35 @@
+import sys
+import re
+
+CONTEXT_SEPARATOR = '|-|'
+FILE_PATTERN = r"^diff --git a/(.+) b/(.+)$"
+BLOCK_PATTERN = r"^@@ [-\+0-9,]+ [-\+0-9,]+ @@ (.+)$"
+
+def main():
+    changes = set()
+    current_file = None
+
+    line = raw_input()
+    while True:
+        m = re.search(FILE_PATTERN, line)
+        if m:
+            changes.add(m.group(1))
+            current_file = m.group(2)
+            changes.add(current_file)
+
+        m = re.search(BLOCK_PATTERN, line)
+        if m:
+            context = m.group(1)
+            if context:
+                changes.add('{}{}{}'.format(current_file, CONTEXT_SEPARATOR, context))
+
+        try:
+            line = raw_input()
+        except EOFError:
+            break
+
+    for change in sorted(list(changes)):
+        print(change)
+
+if __name__ == '__main__':
+    main()
