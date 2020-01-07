eded88ca85368cc253d82f1447874201713f9c6a
Tommi Oinonen <tommi.oinonen@gmail.com>
2020-01-07 11:48:00 +0200

    cleaned debug stuff from pytest fixture tests

--- pytest/fixture_tests/test_setups_and_teardowns.py
+++ pytest/fixture_tests/test_setups_and_teardowns.py
@@ -22,36 +22,15 @@ class TestClassWithFailingTestTearDownAndPassingTests(unittest.TestCase):
 class TestClassWithFailingTestTearDownAndFailingTests(unittest.TestCase):
 
     def test_something(self):
-        print('foo1')
-        self.foo = 1
         assert False
 
     def test_something_else(self):
-        print('foo2')
-        self.foo = 2
         assert False
 
     def tearDown(self):
-        print('foo3')
         assert False
-        if self.foo == 1:
-            assert False
-        else:
-            pass
 
     @classmethod
     def tearDownClass(cls):
-        print('foo4')
         assert False
 
-
-
-
-#@unittest.skip("demonstrating skipping")
-
-#@unittest.skipIf(mylib.__version__ < (1, 3), "not supported in this library version")
-
-#@unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
-
-# @unittest.skip("showing class skipping")
-# class MySkippedTestCase(unittest.TestCase):
