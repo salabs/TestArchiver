0e64dc5b993540327be5fa042dbabfcf893a0647
Sipke Vriend <sipke.vriend@gmail.com>
2020-10-29 15:30:59 +1000

    Junit output xml can have an empty timestamp, so do not adjust it.
    If there is a timestamp adjust it in adjusted_timestamp but if not then
    return None.

--- test_archiver/archiver.py
+++ test_archiver/archiver.py
@@ -717,6 +717,8 @@ def adjusted_timestamp_to_datetime(timestamp, time_adjust_secs=0):
 
 
 def adjusted_timestamp(timestamp, time_adjust_secs=0):
-    adjusted_datetime = adjusted_timestamp_to_datetime(timestamp, time_adjust_secs)
-    adjusted_stamp = adjusted_datetime.isoformat(timespec='milliseconds')
+    adjusted_stamp = timestamp
+    if timestamp:
+        adjusted_datetime = adjusted_timestamp_to_datetime(timestamp, time_adjust_secs)
+        adjusted_stamp = adjusted_datetime.isoformat(timespec='milliseconds')
     return adjusted_stamp
