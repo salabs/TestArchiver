26bd90d4591abe45f3085a9a21c7a5fe92ad1aa8
Tommi Oinonen <tommi.oinonen@siili.com>
2025-08-04 10:04:28 +0300

    Use PDM dynamic scm versioning and update publish workflow

--- src/test_archiver/version.py
+++ src/test_archiver/version.py
@@ -1,21 +1,28 @@
-import os
+# import os
 
-ARCHIVER_VERSION = "3.0.0"
+# ARCHIVER_VERSION = "3.0.0"
 
 
-def dynamic_package_version():
-    version = ARCHIVER_VERSION
-    build_number = None
-    try:
-        build_number = os.environ['BUILD_NUMBER_FOR_DEV_PACKAGE_VERSION']
-    except KeyError:
-        pass
+# def dynamic_package_version():
+#     version = ARCHIVER_VERSION
+#     build_number = None
+#     try:
+#         build_number = os.environ['BUILD_NUMBER_FOR_DEV_PACKAGE_VERSION']
+#     except KeyError:
+#         pass
 
-    if build_number:
-        # Not an official release
-        if 'dev' not in version:
-            # If not dev then release candidate
-            version += 'rc'
-        version += build_number
+#     if build_number:
+#         # Not an official release
+#         if 'dev' not in version:
+#             # If not dev then release candidate
+#             version += 'rc'
+#         version += build_number
 
-    return version
+#     return version
+
+from pathlib import Path
+
+DEFAULT_VERSION = "0.0.1"
+
+version_file = Path(__file__).parent / 'version.txt'
+ARCHIVER_VERSION = version_file.read_text().strip() if version_file.exists() else DEFAULT_VERSION
